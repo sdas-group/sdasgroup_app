bbf91c2b970c4ec4932df453652fbdeed42f6ed1
sdas-group <consultoriaelectrica@mateovergara.com>
2023-08-08 12:37:18 -0500

    engineering lead changed

--- app/service/members/data.py
+++ app/service/members/data.py
@@ -1,5 +1,4 @@
 MEMBER_CHOICES_RE = 20
-MEMBER_CHOICES_IN = 30
 MEMBER_CHOICES_SR = 40
 MEMBER_CHOICES_PDR = 50
 MEMBER_CHOICES_ET = 60
@@ -7,7 +6,6 @@ MEMBER_CHOICES_FS = 70
 MEMBER_CHOICES_HR = 80
 MEMBER_CHOICES_DS = 90
 MEMBER_CHOICES_PR = 100
-MEMBER_CHOICES_AR = 110
 MEMBER_CHOICES_ER = 120
 MEMBER_CHOICES_PS = 130
 MEMBER_CHOICES_MS = 140
@@ -19,12 +17,10 @@ MEMBER_CHOICES_HG = 190
 
 MEMBER_CHOICES = (
     (MEMBER_CHOICES_RE, 'Research Engineer'),
-    (MEMBER_CHOICES_IN, 'Interns'),
     (MEMBER_CHOICES_SR, 'Senior Researchers'),
     (MEMBER_CHOICES_PDR, 'Postdoctoral Researchers'),
     (MEMBER_CHOICES_HR, 'Honorary Research Fellows'),
-    (MEMBER_CHOICES_PR, 'Principal Researchers'),
-    (MEMBER_CHOICES_AR, 'Associate Researchers'),
+    (MEMBER_CHOICES_PR, 'Research Associates'),
     (MEMBER_CHOICES_ER, 'Collaborator Researchers'),
     (MEMBER_CHOICES_PS, 'Research Assistants - Level III'),
     (MEMBER_CHOICES_MS, 'Research Assistants - Level II'),
